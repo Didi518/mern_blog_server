1bae375a2c18494cafaad9a9fbd5fa14fd014a53
Didi518 <kevin.ouali@gmail.com>
2024-11-27 22:51:10 +0100

    :passport_control: cors for frontend

--- middlewares/uploadPictureMiddleware.js
+++ middlewares/uploadPictureMiddleware.js
@@ -6,12 +6,12 @@ cloudinaryConfig
 
 const uploadPicture = async (file) => {
   try {
-    const base64Image = Buffer.from(file.buffer).toString('base64')
+    const base64Image = Buffer.from(file.data).toString('base64')
     const dataURI = `data:${file.mimetype};base64,${base64Image}`
 
     const uploadResponse = await cloudinary.v2.uploader.upload(dataURI, {
       folder: 'mern_blog',
-      public_id: `${Date.now()}-${file.originalname}`,
+      public_id: `${Date.now()}-${file.name}`,
     })
 
     return uploadResponse.url
