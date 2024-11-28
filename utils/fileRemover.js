826b0953527c5dd96e934ed55be5159820a05589
Didi518 <kevin.ouali@gmail.com>
2024-11-28 09:03:51 +0100

    update file remover

--- utils/fileRemover.js
+++ utils/fileRemover.js
@@ -1,7 +1,20 @@
 import cloudinary from 'cloudinary'
 
-const fileRemover = async (publicId) => {
+const extractPublicIdFromUrl = (url) => {
+  const regex = /\/v\d+\/(.*?)\.(jpg|jpeg|png|gif|webp)$/
+  const match = url.match(regex)
+  return match ? match[1] : null
+}
+
+const fileRemover = async (imageUrl) => {
   try {
+    const publicId = extractPublicIdFromUrl(imageUrl)
+
+    if (!publicId) {
+      console.log(`Le public_id n'a pas pu être extrait de l'URL: ${imageUrl}`)
+      return
+    }
+
     const result = await cloudinary.uploader.destroy(publicId)
     if (result.result === 'ok') {
       console.log(`Suppression réussie pour le fichier : ${publicId}`)
@@ -9,10 +22,7 @@ const fileRemover = async (publicId) => {
       console.log(`Erreur : Le fichier ${publicId} n'a pas pu être supprimé.`)
     }
   } catch (error) {
-    console.error(
-      `Erreur lors de la suppression du fichier ${publicId}:`,
-      error.message
-    )
+    console.error(`Erreur lors de la suppression du fichier: ${error.message}`)
   }
 }
 
