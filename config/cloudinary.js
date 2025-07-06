b9ca7c6b1ad6b8095f0e109de5c45cc2450465cb
Didi518 <kevin.ouali@gmail.com>
2025-07-06 19:08:21 +0200

    Fix cloudinary import conflict - clean version

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -99,13 +99,3 @@ export const extractPublicId = (cloudinaryUrl) => {
 };
 
 export { cloudinary };
-
-import cloudinary from 'cloudinary'
-
-const cloudinaryConfig = cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-})
-
-export default cloudinaryConfig
