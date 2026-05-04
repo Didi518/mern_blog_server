f01556b88a8c0275200b044a3c608ba2983c23ce
Didi518 <kevin.ouali@gmail.com>
2026-05-04 20:18:07 +0200

    maj vercel

--- server.js
+++ server.js
@@ -4,10 +4,7 @@ import cors from 'cors';
 
 import connectDB from './config/db.js';
 import { corsOptions } from './config/corsOptions.js';
-import {
-  errorResponseHandler,
-  invalidPathHandler,
-} from './middlewares/errorHandler.js';
+
 import userRoutes from './routes/userRoutes.js';
 import postRoutes from './routes/postRoutes.js';
 import commentRoutes from './routes/commentRoutes.js';
@@ -15,15 +12,12 @@ import postCategoriesRoutes from './routes/postCategoriesRoutes.js';
 
 dotenv.config();
 
-connectDB();
-
 const app = express();
 
 app.use(express.json());
 app.use(cors(corsOptions));
 
-// Plus besoin de express-fileupload avec Cloudinary
-// app.use(fileUpload());
+connectDB();
 
 app.get('/', (_req, res) => {
   res.send('Serveur connecté...');
@@ -34,12 +28,4 @@ app.use('/api/v1/posts', postRoutes);
 app.use('/api/v1/comments', commentRoutes);
 app.use('/api/v1/post-categories', postCategoriesRoutes);
 
-// Les images sont maintenant servies par Cloudinary
-// Plus besoin de servir les fichiers statiques locaux
-
-app.use(invalidPathHandler);
-app.use(errorResponseHandler);
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Serveur connecté sur le port ${PORT}`));
+export default app;
