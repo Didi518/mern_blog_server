eb57eb08ca5b09e32f98629d1f046c72250e8125
Didi518 <kevin.ouali@gmail.com>
2026-05-04 20:38:10 +0200

    maj vercel

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://mern-blog-client-qtvy.onrender.com',
-]
+  'https://mern-blog-client-99y1si0eb-didi518s-projects.vercel.app',
+];
 
-export default allowedOrigins
+export default allowedOrigins;
