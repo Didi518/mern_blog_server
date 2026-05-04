a8c04231534890cd6a035af620d4a6466c0aaaf5
Didi518 <kevin.ouali@gmail.com>
2026-05-04 20:51:03 +0200

    maj vercel

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,26 +1,29 @@
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 
-import User from "../models/User.js";
+import User from '../models/User.js';
 
-export const authGuard = async (req, _res, next) => {
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    try {
-      const token = req.headers.authorization.split(" ")[1];
-      const { id } = jwt.verify(token, process.env.JWT_SECRET_KEY);
-      req.user = await User.findById(id).select("-password");
-      next();
-    } catch (error) {
-      const err = new Error("Non autorisé, Echec  du token");
-      err.statusCode = 401;
-      next(err);
+export const authGuard = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return next(new Error('Non autorisé, Pas de token'));
+  }
+
+  try {
+    const token = authHeader.split(' ')[1];
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    const user = await User.findById(decoded.id).select('-password');
+
+    if (!user) {
+      return next(new Error('Utilisateur introuvable'));
     }
-  } else {
-    let error = new Error("Non autorisé, Pas de token");
-    error.statusCode = 401;
-    next(error);
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return next(new Error('Token invalide ou expiré'));
   }
 };
 
@@ -28,7 +31,7 @@ export const adminGuard = async (req, _res, next) => {
   if (req.user && req.user.admin) {
     next();
   } else {
-    let error = new Error("Non autorisé, action réservée aux administrateurs");
+    let error = new Error('Non autorisé, action réservée aux administrateurs');
     error.statusCode = 401;
     next(error);
   }
